068405e5a2da9b0e549b90fd683f02054561ae97
David Galvan <dgalvan@wgu.edu>
2020-02-26 11:36:07 -0800

    Add project files.

---
+++ Spark/newtransaction.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Spark
+{
+    public partial class newtransaction : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private decimal CalculateVAT(decimal Amount)
+        {
+            decimal VATAmount = Amount * 0.2m;
+            VATAmount = Math.Round(VATAmount, 2);
+            return VATAmount;
+        }
+
+        protected void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
